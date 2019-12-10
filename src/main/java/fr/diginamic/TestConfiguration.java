5f2ac743d3210d2bd0bdf37852b30afe05b2c9c6
Zhanna <j.santybay@gmail.com>
2019-12-10 10:22:08 +0100

    TP1 : JDBC

--- src/main/java/fr/diginamic/TestConfiguration.java
+++ src/main/java/fr/diginamic/TestConfiguration.java
@@ -1,13 +1,20 @@
 package fr.diginamic;
 
+import java.util.Enumeration;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class TestConfiguration {
 
 	public static void main(String[] args) {
 		ResourceBundle myConfig = ResourceBundle.getBundle("demo-jdbc");
-		String driverName = myConfig.getString("database.driver");
-		System.out.println(driverName);
+			
+		Enumeration<String> keys = myConfig.getKeys(); 
+		while (keys.hasMoreElements()) {
+			String key=keys.nextElement();
+	         System.out.println(key+" "+myConfig.getString(key));
+	      }
+		
 	}
 
 }
